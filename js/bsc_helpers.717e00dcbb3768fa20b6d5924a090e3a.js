f2b31f551c9a24f27fe3dbbbddeb6608f6480a7c
Steven Enamakel <enamakel@vezures.com>
2021-02-20 14:33:09 +0300

    Updates

---
+++ js/bsc_helpers.717e00dcbb3768fa20b6d5924a090e3a.js
@@ -0,0 +1,350 @@
+/*!
+* YieldFarming
+* Boilerplate for a Static website using EJS and SASS
+* https://yieldfarming.info
+* @author Jongseung Lim -- https://yieldfarming.info
+* Copyright 2021. MIT Licensed.
+*/
+
+async function getBscUniPool(App, pool, poolAddress, stakingAddress) {    
+    let q0, q1;
+    const reserves = await pool.getReserves();
+    q0 = reserves._reserve0;
+    q1 = reserves._reserve1;
+    const decimals = await pool.decimals();
+    const token0 = await pool.token0();
+    const token1 = await pool.token1();
+    return { 
+        symbol : await pool.symbol(),
+        name : await pool.name(),
+        address: poolAddress,
+        token0,
+        q0,
+        token1,
+        q1,
+        totalSupply: await pool.totalSupply() / 10 ** decimals,
+        stakingAddress: stakingAddress,
+        staked: await pool.balanceOf(stakingAddress) / 10 ** decimals,
+        decimals: decimals,
+        unstaked: await pool.balanceOf(App.YOUR_ADDRESS) / 10 ** decimals,
+        contract: pool,
+        tokens : [token0, token1],
+        is1inch : false
+    };
+}
+
+async function getBep20(App, token, address, stakingAddress) {
+    if (address == "0x0000000000000000000000000000000000000000") {
+      return {
+        address,
+        name : "Binance",
+        symbol : "BNB",
+        totalSupply: 1e8,
+        decimals: 18,
+        staked: 0,
+        unstaked: 0,
+        contract: null,
+        tokens:[address]
+      }
+    }
+    const decimals = await token.decimals()
+    return {
+        address,
+        name : await token.name(),
+        symbol : await token.symbol(),
+        totalSupply : await token.totalSupply(),
+        decimals : decimals,
+        staked:  await token.balanceOf(stakingAddress) / 10 ** decimals,
+        unstaked: await token.balanceOf(App.YOUR_ADDRESS)  / 10 ** decimals,
+        contract: token,
+        tokens : [address]
+    };
+}
+
+async function getBscStoredToken(App, tokenAddress, stakingAddress, type) {
+  switch (type) {
+    case "uniswap": 
+      const pool = new ethers.Contract(tokenAddress, UNI_ABI, App.provider);
+      return await getBscUniPool(App, pool, tokenAddress, stakingAddress);
+    case "bep20":
+      const bep20 = new ethers.Contract(tokenAddress, ERC20_ABI, App.provider);
+      return await getBep20(App, bep20, tokenAddress, stakingAddress);
+  }
+}
+
+async function getBscToken(App, tokenAddress, stakingAddress) {
+    if (tokenAddress == "0x0000000000000000000000000000000000000000") {
+      return getBep20(App, null, tokenAddress, "")
+    }
+    const type = window.localStorage.getItem(tokenAddress);
+    if (type) return getBscStoredToken(App, tokenAddress, stakingAddress, type);
+    try {
+      const pool = new ethers.Contract(tokenAddress, UNI_ABI, App.provider);
+      const _token0 = await pool.token0();
+      const uniPool = await getBscUniPool(App, pool, tokenAddress, stakingAddress);
+      window.localStorage.setItem(tokenAddress, "uniswap");
+      return uniPool;
+    }
+    catch(err) {
+    }
+    try {
+      const bep20 = new ethers.Contract(tokenAddress, ERC20_ABI, App.provider);
+      const _name = await bep20.name();
+      const bep20tok = await getBep20(App, bep20, tokenAddress, stakingAddress);
+      window.localStorage.setItem(tokenAddress, "bep20");
+      return bep20tok;
+    }
+    catch(err) {
+      console.log(`Couldn't match ${tokenAddress} to any known token type.`);
+    }
+  }
+
+async function loadBscSynthetixPoolInfo(App, tokens, prices, stakingAbi, stakingAddress,
+    rewardTokenFunction, stakeTokenFunction) {
+      const STAKING_POOL = new ethers.Contract(stakingAddress, stakingAbi, App.provider);
+  
+      if (!STAKING_POOL.callStatic[stakeTokenFunction]) {
+        console.log("Couldn't find stake function ", stakeTokenFunction);
+      }
+      const stakeTokenAddress = await STAKING_POOL.callStatic[stakeTokenFunction]();
+  
+      const rewardTokenAddress = await STAKING_POOL.callStatic[rewardTokenFunction]();
+  
+      var stakeToken = await getBscToken(App, stakeTokenAddress, stakingAddress);
+  
+      if (stakeTokenAddress.toLowerCase() === rewardTokenAddress.toLowerCase()) {
+        stakeToken.staked = await STAKING_POOL.totalSupply() / 10 ** stakeToken.decimals;
+      }
+  
+      var newPriceAddresses = stakeToken.tokens.filter(x =>
+        x.toLowerCase() !=  "0xb34ab2f65c6e4f764ffe740ab83f982021faed6d" && //BSG can't be retrieved from Coingecko
+        !getParameterCaseInsensitive(prices, x));
+      var newPrices = await lookUpTokenPrices(newPriceAddresses);
+      for (const key in newPrices) {
+        if (newPrices[key]?.usd)
+            prices[key] = newPrices[key];
+      }
+      var newTokenAddresses = stakeToken.tokens.filter(x =>
+        !getParameterCaseInsensitive(tokens,x));
+      for (const address of newTokenAddresses) {
+          tokens[address] = await getBscToken(App, address, stakingAddress);
+      }
+      if (!getParameterCaseInsensitive(tokens, rewardTokenAddress)) {
+          tokens[rewardTokenAddress] = await getBScToken(App, rewardTokenAddress, stakingAddress);
+      }
+      const rewardToken = getParameterCaseInsensitive(tokens, rewardTokenAddress);
+  
+      const rewardTokenTicker = rewardToken.symbol;
+  
+      const poolPrices = getPoolPrices(tokens, prices, stakeToken);
+  
+      const stakeTokenTicker = poolPrices.stakeTokenTicker;
+  
+      const stakeTokenPrice =
+          prices[stakeTokenAddress]?.usd ?? getParameterCaseInsensitive(prices, stakeTokenAddress)?.usd;
+      const rewardTokenPrice = getParameterCaseInsensitive(prices, rewardTokenAddress)?.usd;
+  
+      const periodFinish = await STAKING_POOL.periodFinish();
+      const rewardRate = await STAKING_POOL.rewardRate();
+      const weeklyRewards = (Date.now() / 1000 > periodFinish) ? 0 : rewardRate / 1e18 * 604800;
+  
+      const usdPerWeek = weeklyRewards * rewardTokenPrice;
+  
+      const staked_tvl = poolPrices.staked_tvl;
+  
+      const userStaked = await STAKING_POOL.balanceOf(App.YOUR_ADDRESS) / 10 ** stakeToken.decimals;
+  
+      const userUnstaked = stakeToken.unstaked;
+  
+      const earned = await STAKING_POOL.earned(App.YOUR_ADDRESS) / 10 ** rewardToken.decimals;
+  
+      return  {
+        stakingAddress,
+        poolPrices,
+        stakeTokenAddress,
+        rewardTokenAddress,
+        stakeTokenTicker,
+        rewardTokenTicker,
+        stakeTokenPrice,
+        rewardTokenPrice,
+        weeklyRewards,
+        usdPerWeek,
+        staked_tvl,
+        userStaked,
+        userUnstaked,
+        earned
+      }
+}
+
+async function loadBscSynthetixPool(App, tokens, prices, abi, address, rewardTokenFunction, stakeTokenFunction) {
+    const info = await loadBscSynthetixPoolInfo(App, tokens, prices, abi, address, rewardTokenFunction, stakeTokenFunction);
+    return await printSynthetixPool(App, info);
+}
+
+async function loadBscBasisFork(data) {
+    const App = await init_ethers();
+
+    _print(`Initialized ${App.YOUR_ADDRESS}`);
+    _print("Reading smart contracts...\n");
+
+    var tokens = {};
+    var prices = {"0x55d398326f99059ff775485246999027b3197955" : { usd : 1 }};
+    var totalStaked = 0;
+    
+    let p1 = await loadBscSynthetixPool(App, tokens, prices, data.PoolABI, 
+        data.SharePool.address, data.SharePool.rewardToken, data.SharePool.stakeToken);
+    totalStaked += p1.staked_tvl;
+    
+    if (data.SharePool2) {
+      let p3 = await loadBscSynthetixPool(App, tokens, prices, data.PoolABI, 
+          data.SharePool2.address, data.SharePool2.rewardToken, data.SharePool2.stakeToken);
+      totalStaked += p3.staked_tvl;
+    }
+
+    let p2 = await loadBscSynthetixPool(App, tokens, prices, data.PoolABI, 
+        data.CashPool.address, data.CashPool.rewardToken, data.CashPool.stakeToken);
+    totalStaked += p2.staked_tvl;
+
+    if (data.SeedBanks) {
+      let p = await loadMultipleBscSynthetixPools(App, tokens, prices, data.SeedBanks)
+      totalStaked += p.staked_tvl;
+      if (p.totalUserStaked > 0) {
+        _print(`You are staking a total of $${formatMoney(p.totalUserStaked)} at an APY of ${(p.totalApy * 100).toFixed(2)}%\n`);
+      }
+    }
+
+    if (!data.SeedBanks)
+    {
+      if (data.Boardrooms) {
+        for (const boardroom of data.Boardrooms) {
+          let br = await loadBoardroom(App, prices, boardroom.address, data.Oracle, data.UniswapLP, data.Cash,
+              data.ShareTicker, data.CashTicker, data.ExpansionsPerDay, data.MaximumExpansion, 
+              data.Decimals, boardroom.ratio, data.TargetMantissa);
+          totalStaked += br.staked_tvl;
+        }
+      }
+      else {
+        let br = await loadBoardroom(App, prices, data.Boardroom, data.Oracle, data.UniswapLP, data.Cash,
+            data.ShareTicker, data.CashTicker, data.ExpansionsPerDay, data.MaximumExpansion, 
+            data.Decimals, 1, data.TargetMantissa);
+        totalStaked += br.staked_tvl;
+      }
+    } 
+
+    _print_bold(`Total staked: $${formatMoney(totalStaked)}`)
+
+    hideLoading();
+}
+
+
+async function getBscPoolInfo(app, chefContract, chefAddress, poolIndex, pendingRewardsFunction) {  
+  const poolInfo = await chefContract.poolInfo(poolIndex);
+  if (poolInfo.allocPoint == 0) {
+    return {
+      address: poolInfo.lpToken,
+      allocPoints: poolInfo.allocPoint ?? 1,
+      poolToken: null,
+      userStaked : 0,
+      pendingRewardTokens : 0,
+      stakedToken : null,
+      userLPStaked : 0,
+      lastRewardBlock : poolInfo.lastRewardBlock
+    };
+  }
+  const poolToken = await getBscToken(app, poolInfo.lpToken, chefAddress);
+  const userInfo = await chefContract.userInfo(poolIndex, app.YOUR_ADDRESS);
+  const pendingRewardTokens = await chefContract.callStatic[pendingRewardsFunction](poolIndex, app.YOUR_ADDRESS);
+  const staked = userInfo.amount / 10 ** poolToken.decimals;
+  var stakedToken;
+  var userLPStaked;
+  if (poolInfo.stakedHoldableToken != null && 
+    poolInfo.stakedHoldableToken != "0x0000000000000000000000000000000000000000") {
+    stakedToken = await getBscToken(app, poolInfo.stakedHoldableToken, chefAddress);
+    userLPStaked = userInfo.stakedLPAmount / 10 ** poolToken.decimals
+  }
+  return {
+      address: poolInfo.lpToken,
+      allocPoints: poolInfo.allocPoint ?? 1,
+      poolToken: poolToken,
+      userStaked : staked,
+      pendingRewardTokens : pendingRewardTokens / 10 ** 18,
+      stakedToken : stakedToken,
+      userLPStaked : userLPStaked,
+      lastRewardBlock : poolInfo.lastRewardBlock
+  };
+}
+
+async function loadBscChefContract(App, tokens, prices, chef, chefAddress, chefAbi, rewardTokenTicker,
+  rewardTokenFunction, rewardsPerBlockFunction, rewardsPerWeekFixed, pendingRewardsFunction,
+  deathPoolIndices) {
+  const chefContract = chef ?? new ethers.Contract(chefAddress, chefAbi, App.provider);
+
+  const poolCount = parseInt(await chefContract.poolLength(), 10);
+  const totalAllocPoints = await chefContract.totalAllocPoint();
+
+  _print(`Found ${poolCount} pools.\n`)
+
+  _print(`Showing incentivized pools only.\n`);
+
+  var tokens = {};
+
+  const rewardTokenAddress = await chefContract.callStatic[rewardTokenFunction]();
+  const rewardToken = await getBscToken(App, rewardTokenAddress, chefAddress);
+  const rewardsPerWeek = rewardsPerWeekFixed ?? 
+    await chefContract.callStatic[rewardsPerBlockFunction]() 
+    / 10 ** rewardToken.decimals * 604800 / 3
+
+  const poolInfos = await Promise.all([...Array(poolCount).keys()].map(async (x) =>
+    await getBscPoolInfo(App, chefContract, chefAddress, x, pendingRewardsFunction)));
+
+  var tokenAddresses = [].concat.apply([], poolInfos.filter(x => x.poolToken).map(x => x.poolToken.tokens));
+
+  await Promise.all(tokenAddresses.map(async (address) => {
+      tokens[address] = await getBscToken(App, address, chefAddress);
+  }));
+
+  if (deathPoolIndices) {   //load prices for the deathpool assets
+    deathPoolIndices.map(i => poolInfos[i])
+                     .map(poolInfo => 
+      poolInfo.poolToken ? getPoolPrices(tokens, prices, poolInfo.poolToken) : undefined);
+  }
+
+  const poolPrices = poolInfos.map(poolInfo => poolInfo.poolToken ? getPoolPrices(tokens, prices, poolInfo.poolToken) : undefined);
+
+
+  _print("Finished reading smart contracts.\n");
+    
+  for (i = 0; i < poolCount; i++) {
+    if (poolPrices[i]) {
+      printChefPool(App, chefAbi, chefAddress, prices, tokens, poolInfos[i], i, poolPrices[i],
+        totalAllocPoints, rewardsPerWeek, rewardTokenTicker, rewardTokenAddress,
+        pendingRewardsFunction);
+    }
+  }
+}
+
+
+const bscTokens = [ 
+  { "id": "wbnb", "symbol": "wbnb","contract": "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c" }, 
+  {"id": "binance-usd", "symbol": "busd", "contract": "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56"  }, 
+  { "id": "pancakeswap-token", "symbol": "CAKE", "contract": "0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82"  }, 
+  {"id": "beefy-finance", "symbol": "BIFI", "contract": "0xca3f508b8e4dd382ee878a314789373d80a5190a" }, 
+  {"id": "bdollar-share", "symbol": "sBDO", "contract": "0x0d9319565be7f53cefe84ad201be3f40feae2740"  }, 
+  { "id": "belugaswap","symbol": "BELUGA", "contract": "0x181de8c57c4f25eba9fd27757bbd11cc66a55d31" }, 
+  { "id": "chainlink","symbol": "LINK","contract":"0xf8a0bf9cf54bb92f17374d9e9a321e6a111a51bd" }, 
+  { "id": "bscex","symbol": "BSCX", "contract": "0x5ac52ee5b2a633895292ff6d8a89bb9190451587" },
+  { "id": "binance-eth","symbol": "BETH", "contract": "0x250632378e573c6be1ac2f97fcdf00515d0aa91b" },
+  { "id": "tether","symbol": "USDT", "contract": "0x55d398326f99059fF775485246999027B3197955" },
+  { "id": "bitcoin-bep2","symbol": "BTCB", "contract": "0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c" },
+  { "id": "ethereum","symbol": "ETH", "contract": "0x2170Ed0880ac9A755fd29B2688956BD959F933F8" },
+  { "id": "bakerytoken","symbol": "BAKE", "contract": "0xE02dF9e3e622DeBdD69fb838bB799E3F168902c5" },
+]
+
+async function getBscPrices() {
+  const idPrices = await lookUpPrices(bscTokens.map(x => x.id));
+  const prices = {}
+  for (const bt of bscTokens)
+      if (idPrices[bt.id])
+          prices[bt.contract] = idPrices[bt.id];
+  return prices;
+}
